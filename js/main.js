2f785a6f6b28a4d69f216fff1252607dea2f4d6c
chris <mentachris@hotmail.fr>
2024-01-16 16:22:31 +0100

    portable suite et presque fin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const burgerButton = document.getElementById("burger");
+const closeBtn = document.getElementById("closeBtn")
 const mainMenu = document.getElementsByClassName("main-menu")[0];
 
-burgerButton.addEventListener ("click", function(){
+
+burgerButton.addEventListener("click", function(){
     mainMenu.style.display ="block";
 });
+
+closeBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    mainMenu.style.display ="none";
+});
+
 
